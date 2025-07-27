07003f038320e14267b8fea99d360bbe434e0a2c
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-07-27 13:34:25 +0530

    commit

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-import turtle
-
-turtle.Screen().bgcolor("Orange")
-board = turtle.Turtle()
- 
-# first triangle for star
-board.forward(100) # draw base
- 
-board.left(120)
-board.forward(100)
- 
-board.left(120)
-board.forward(100)
- 
-board.penup()
-board.right(150)
-board.forward(50)
- 
-# second triangle for star
-board.pendown()
-board.right(90)
-board.forward(100)
- 
-board.right(120)
-board.forward(100)
- 
-board.right(120)
-board.forward(100)
- 
-turtle.done()
+import turtle     #importing library       
+my_wn = turtle.Screen()
+my_wn.bgcolor("light blue") #screen background color
+my_wn.title("Turtle")
+my_pen = turtle.Turtle()
+size = 1
+while True: #iterate loop
+  for i in range(4): 
+    my_pen.fd(size)
+    my_pen.left(90)
+    size = size + 1
+  size = size + 1
